9f351401e7cb754efed7cfec188c191e05dd994d
William Okuno <38401861+wokuno@users.noreply.github.com>
2025-12-03 21:22:48 -0600

    better ports

--- src/main.go
+++ src/main.go
@@ -14,6 +14,6 @@ func main() {
 	http.HandleFunc("/api/menu/stream", menuApp.StreamMenu)
 	http.Handle("/", http.FileServer(http.Dir("/app/static")))
 
-	fmt.Println("Server starting on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server starting on http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
